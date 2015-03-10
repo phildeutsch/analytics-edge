19077424d944248dc763f41525038725e6b21d3a
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2015-03-10 15:39:45 +0000

    Finished quick questions

--- week2/week2.R
+++ week2/week2.R
@@ -32,4 +32,10 @@ players = cbind(players, p = predict(RunsReg, players))
 players = merge(players, players, by = NULL)
 players$total = players$p.x + players$p.y
 players = players[(players$Name.x!=players$Name.y) & (players$Salary.x + players$Salary.y)<=1.5,]
-players[order(-players$total),]
+head(players[order(-players$total),c("Name.x", "Name.y")],1)
+
+teamRank = c(1,2,3,3,4,4,4,4,5,5)
+wins2012 = c(94,88,95,88,93,94,98,97,93,94)
+wins2013 = c(97,97,92,93,92,96,94,96,92,90)
+cor(teamRank, wins2012)
+cor(teamRank, wins2013)
