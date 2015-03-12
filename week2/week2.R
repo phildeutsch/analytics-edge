5846fbf7416d0caf601cf3591f915d431ba1f3a2
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2015-03-12 11:21:01 +0000

    Typo

--- week2/week2.R
+++ week2/week2.R
@@ -1,3 +1,6 @@
+require(zoo)
+require(caret)
+
 ## Quick questions
 
 ## 1
@@ -58,12 +61,14 @@ stepfit = step(lm(Temp ~ . - Year - Month, training))
 summary(stepfit)
 summary(lm(Temp ~ . - Year - Month, training))
 
-p = predict(stepfi, testing)
+p = predict(stepfit, testing)
 
 TSS = with(testing, sum((Temp-mean(training$Temp))**2)) # not sure why we use the training mean
 RSS = with(testing, sum((Temp-p)**2))
 R2  = 1 - RSS/TSS
 R2
+
+R2(p, testing$Temp, formula="traditional")
 
 ## Reading test scores
 pisaTrain = read.csv("pisa2009train.csv")
@@ -102,3 +107,41 @@ TSS
 
 
 1-RSS/TSS
+
+## Flu epidemics
+
+FluTrain = read.csv("FluTrain.csv")
+FluTrain[which.max(FluTrain$ILI),]
+FluTrain[which.max(FluTrain$Queries),]
+
+hist(FluTrain$ILI, breaks=50)
+
+plot(log(ILI) ~ Queries, FluTrain)
+
+FluTrend1 = lm(log(ILI) ~ Queries, FluTrain)
+summary(FluTrend1)
+
+FluTest = read.csv("FluTest.csv")
+PredTest1 = cbind(FluTest, p=exp(predict(FluTrend1, newdata=FluTest)))
+head(PredTest1, 20)
+
+with(PredTest1, sqrt(sum((ILI-p)**2)/nrow(PredTest1)))
+
+ILILag2 = lag(zoo(FluTrain$ILI), -2, na.pad=TRUE)
+FluTrain$ILILag2 = coredata(ILILag2)
+sum(is.na(FluTrain$ILILag2))
+
+plot(ILILag2 ~ ILI, FluTrain)
+
+FluTrend2 = lm(log(ILI) ~ Queries + log(ILILag2), FluTrain)
+summary(FluTrend2)
+
+ILILag2 = lag(zoo(FluTest$ILI), -2, na.pad=TRUE)
+FluTest$ILILag2 = coredata(ILILag2)
+
+FluTest$ILILag2[1] = FluTrain$ILI[416]
+FluTest$ILILag2[2] = FluTrain$ILI[417]
+head(FluTest)
+p = exp(predict(FluTrend2, FluTest))
+RSS = sum((p-FluTest$ILI)**2)
+sqrt(RSS/nrow(FluTest))
